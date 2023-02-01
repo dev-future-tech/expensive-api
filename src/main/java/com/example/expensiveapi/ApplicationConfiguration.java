a948bae9592978b4b5e7d45c358d1dfba827c012
Anthony Ikeda <anthony.ikeda@gmail.com>
2023-01-31 20:09:13 -0800

    Injected RestTemplate (temporarily) into Data Handler to test injection

--- src/main/java/com/example/expensiveapi/ApplicationConfiguration.java
+++ src/main/java/com/example/expensiveapi/ApplicationConfiguration.java
@@ -2,8 +2,13 @@ package com.example.expensiveapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ApplicationConfiguration {
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
--- src/main/java/com/example/expensiveapi/ExpensiveDataHandler.java
+++ src/main/java/com/example/expensiveapi/ExpensiveDataHandler.java
@@ -4,15 +4,30 @@ import jakarta.persistence.PostPersist;
 import jakarta.persistence.PrePersist;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
+@Component
 public class ExpensiveDataHandler {
 
+    protected RestTemplate template;
+
     private final Logger log = LoggerFactory.getLogger(ExpensiveDataHandler.class);
+
+    public ExpensiveDataHandler(RestTemplate template) {
+        this.template = template;
+    }
+
+    public ExpensiveDataHandler() {
+
+    }
 
     @PrePersist
     protected void onBeforeCreate(Object entity) {
         Expense expense = (Expense) entity;
         log.debug("Saving expense {}", expense.getExpenseName());
+        assert template != null;
     }
 
     @PostPersist
